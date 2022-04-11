2ecb437d07e835d3296af709045d826be44c0e3f
zaenarifputra <zaenarifputraainurdin@gmail.com>
2022-04-11 12:33:11 +0700

    receipt

---
+++ app/src/main/java/com/example/chakra/receipt_empty.kt
@@ -0,0 +1,11 @@
+package com.example.chakra
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class receipt_empty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receipt_empty)
+    }
+}
