54916a1a2f3e48d01860cda79307fefb785a62d7
zaenarifputra <zaenarifputraainurdin@gmail.com>
2022-04-17 12:35:15 +0700

    Add layout detail history topup

---
+++ app/src/main/java/com/example/chakra/DetailHistoryTopUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chakra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailHistoryTopUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_history_top_up)
+    }
+}
